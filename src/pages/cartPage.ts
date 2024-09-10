fe316e34b797081b97aec64662c6a361b74e443a
matheusdouglas <matheusdouglas1001@gmail.com>
2024-09-10 18:27:36 -0300

    Refactor getProductNames

--- src/pages/cartPage.ts
+++ src/pages/cartPage.ts
@@ -1,10 +1,18 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from "@playwright/test";
 
 export class CartPage {
+  constructor(private page: Page) {}
 
-    constructor(private page: Page) {}
+  async getProductNames(): Promise<string[]> {
+    // Usa 'this.page' para acessar a página atual
+    const locator = this.page.locator(".inventory_item_name");
 
-    async getProductNames() {
-        return await this.page.$$eval('.inventory_item_name', elements => elements.map(el => el.textContent));
-    }
+    // Usa evaluateAll para extrair o texto de todos os elementos encontrados
+    const itemNames = await locator.evaluateAll((elements) =>
+      elements.map((el) => el.textContent ?? "")
+    );
+
+    // Retorna os nomes dos itens
+    return itemNames;
+  }
 }
